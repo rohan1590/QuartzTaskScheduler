15eb3b1487c88c31a38bab710332f3add1b86c92
310169238 <310169238@USDANRMMD1NBP04>
2016-12-14 16:21:38 +0530

    Add project files.

---
+++ src/QuartzTaskScheduler/HelloWorldJob.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quartz;
+using Common.Logging;
+
+
+namespace QuartzTaskScheduler
+{
+    public class HelloWorldJob : IJob
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(HelloWorldJob));
+
+        /// <summary> 
+        /// Empty constructor for job initilization
+        /// <para>
+        /// Quartz requires a public empty constructor so that the
+        /// scheduler can instantiate the class whenever it needs.
+        /// </para>
+        /// </summary>
+        public HelloWorldJob()
+        {
+
+        }
+
+        //public Task Execute(IJobExecutionContext context)
+        Task IJob.Execute(IJobExecutionContext context)
+        {
+            try
+            {
+               
+                Console.WriteLine("Executing : "+ DateTime.Now.ToString("HH:mm:ss"));
+            }
+            catch (NotImplementedException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return null;
+        }
+    }
+}
---
+++ src/QuartzTaskScheduler/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace QuartzTaskScheduler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("task Scheduler Begins");
+
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
+
+            // get a scheduler, start the schedular before triggers or anything else
+            IScheduler sched = schedFact.GetScheduler().Result;
+            sched.Start();
+
+            // create job
+            IJobDetail job = JobBuilder.Create<HelloWorldJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+            // create trigger
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .WithSimpleSchedule(x => x.WithIntervalInSeconds(2).RepeatForever())
+                .Build();
+
+            // Schedule the job using the job and trigger 
+            sched.ScheduleJob(job, trigger);
+
+
+            Console.ReadLine();
+
+            Console.WriteLine("Program ends successfully");
+        }
+    }
+}
